a58fa53023d8b93193ffe89e9f97ffe46c72130d
sparwan1 <sparwan1@asu.edu>
2024-06-02 03:29:05 -0700

    Updates

---
+++ precache-manifest.8f6c891369c6a9d85bc77418e35eafb8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "179d9ea378a4b36a5eae86e354224900",
+    "url": "/react-frontend-dev-portfolio/index.html"
+  },
+  {
+    "revision": "1e1f8b6671128cccc4ec",
+    "url": "/react-frontend-dev-portfolio/static/css/2.22991453.chunk.css"
+  },
+  {
+    "revision": "7d1475daff8399681c18",
+    "url": "/react-frontend-dev-portfolio/static/css/main.3573735d.chunk.css"
+  },
+  {
+    "revision": "1e1f8b6671128cccc4ec",
+    "url": "/react-frontend-dev-portfolio/static/js/2.e4d191c9.chunk.js"
+  },
+  {
+    "revision": "46a379a572333860f78765c743603e43",
+    "url": "/react-frontend-dev-portfolio/static/js/2.e4d191c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d1475daff8399681c18",
+    "url": "/react-frontend-dev-portfolio/static/js/main.b25affc0.chunk.js"
+  },
+  {
+    "revision": "0c71dbb3b6aa4f86725d",
+    "url": "/react-frontend-dev-portfolio/static/js/runtime-main.89d7af9d.js"
+  }
+]);
